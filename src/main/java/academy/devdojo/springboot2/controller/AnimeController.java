5d5cd34cd90fd92c6f339d2e349b0edb454add2c
Carlos Augusto <augustino2003@hotmail.com>
2021-12-18 22:39:46 -0200

    Método GET e findById, Alterado application.properties para application.yml

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -3,7 +3,10 @@ package academy.devdojo.springboot2.controller;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,9 +28,14 @@ public class AnimeController {
 	
 
 	@GetMapping
-	public List<Anime> list(){
+	public ResponseEntity<List<Anime>> list(){
 		log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-		return animeService.listAll();
+		return ResponseEntity.ok((animeService.listAll()));
+	}
+	
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<Anime> findById(@PathVariable Long id){
+		return ResponseEntity.ok((animeService.findById(id)));
 	}
 	
 }
