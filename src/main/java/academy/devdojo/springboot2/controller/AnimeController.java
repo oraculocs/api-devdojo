eec904c2bed305015c08530e51cf003f085fe27e
Carlos Augusto <augustino2003@hotmail.com>
2021-12-25 20:25:04 -0200

    Método Delete, Classes alteradas Controller e Service

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,5 +44,11 @@ public class AnimeController {
 	@PostMapping
 	public ResponseEntity<Anime> save(@RequestBody Anime anime){
 		return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		animeService.delete(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
