4ce7d7cfb92a9760eaa30dc3e720c958b4111c9b
Carlos Augusto <augustino2003@hotmail.com>
2021-12-25 20:11:01 -0200

    Método POST, Classes Controller e Service alteradas

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -1,6 +1,8 @@
 package academy.devdojo.springboot2.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -11,7 +13,11 @@ import academy.devdojo.springboot2.domain.Anime;
 @Service
 public class AnimeService {
 	
-	private List<Anime> animes = List.of(new Anime(1L, "Naruto"), new Anime(2L, "Yu yu Hakusho"));
+	private static List<Anime> animes; 
+	
+	static {
+		animes = new ArrayList<>(List.of(new Anime(1L, "Naruto"), new Anime(2L, "Yu yu Hakusho")));
+	}
 	
 	public List<Anime> listAll(){
 		return animes;
@@ -22,5 +28,11 @@ public class AnimeService {
 					 .filter(anime -> anime.getId().equals(id))
 					 .findFirst()
 					 .orElseThrow(()-> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+	}
+
+	public Anime save(Anime anime) {
+		anime.setId(ThreadLocalRandom.current().nextLong(3, 100000));
+		animes.add(anime);
+		return anime;
 	}
 }
